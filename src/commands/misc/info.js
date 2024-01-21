4c962163770dab1ca1695f00c7cd2ffa61c8585d
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-01-21 12:03:41 +0700

    adding database

---
+++ src/commands/misc/info.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "info",
+    description: "Information about the game Knight",
+    callback: (client, interaction) => {
+        interaction.reply(`This is the game Knight (still in development)`);
+    },
+}
